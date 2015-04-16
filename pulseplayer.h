1036480253ede59809d466a536974f9ca3cb2f61
Daewon.Park <Daewon.Park@windriver.com>
2015-04-16 18:31:57 +0900

    AudioManager Monitor: Add license & copyright
    Add license & copyright in source code.
    Add LICENSE file
    
    Signed-off-by: Daewon.Park <Daewon.Park@windriver.com>

--- pulseplayer.h
+++ pulseplayer.h
@@ -1,3 +1,18 @@
+/* SPDXLicenseID: MPL-2.0
+*
+* Copyright (C) 2014, GENIVI Alliance
+*
+* This file is part of AudioManager Monitor
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License (MPL), v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+* For further information see http://www.genivi.org/.
+*
+* List of changes:
+*/
+
 #ifndef PULSEPLAYER_H
 #define PULSEPLAYER_H
 
